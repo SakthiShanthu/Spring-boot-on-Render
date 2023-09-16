6de1c65769793b8e9f2e5d91b88a4cac6a440498
shanthinip <shanthini.packirisamy@ainqa.com>
2023-09-16 15:52:51 +0530

    Calculator UI completed

--- src/main/java/com/techzone/render/controller/PageController.java
+++ src/main/java/com/techzone/render/controller/PageController.java
@@ -1,12 +1,15 @@
 package com.techzone.render.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 public class PageController {
 
-	@GetMapping("/log")
+	@GetMapping("/first")
 	public String LoginPage() {
 		return "index";
 	}
